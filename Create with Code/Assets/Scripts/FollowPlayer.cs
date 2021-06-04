92748e73597b01bcf9021ebf8d198db9c6b754e3
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-03 20:34:30 -0600

    User can switch between inside the vehicle, behind the vehicle and a side view of the vehicle

--- Create with Code/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Assets/Scripts/FollowPlayer.cs
@@ -6,12 +6,52 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -8);
+    
+    public int viewNumber = 1;
+    private Vector3[] cameraViews = {
+        new Vector3(0, 5, -8), // Behind
+        new Vector3(0, 4.2f, 0), // Inside
+        new Vector3(-14, 4, 0) // Side        
+    };
+
+    //private Vector3 offset;
+    private bool isAllowedToRotate = true;
+    private bool doRotateBack = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Before viewNumber:" + viewNumber);
+            viewNumber = ++viewNumber % cameraViews.Length;
+            Debug.Log("After viewNumber:" + viewNumber);
+        }
+    }
 
     // Update is called once per frame
     //void Update()
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (viewNumber == 2)
+        {
+            if (isAllowedToRotate)
+            {
+                Debug.Log("viewNumber:" + viewNumber);
+                isAllowedToRotate = false;
+                doRotateBack = true;
+                transform.Rotate(Vector3.up, 90.0f);
+            }
+        } else
+        {
+            if (doRotateBack)
+            {
+                transform.Rotate(Vector3.up, -90.0f);
+                doRotateBack = false;
+            }
+            isAllowedToRotate = true;
+        }
+        
+        transform.position = player.transform.position + cameraViews[viewNumber];
+       
     }
 }
