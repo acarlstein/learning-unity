7444cd92006c0597852dd78a889fd784175cc481
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-05 15:49:36 -0600

    Destroy projectile if passes top and lower boundries.

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private readonly float topBound = 40.0f;
+    private readonly float lowerBound = -10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound
+            || transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }        
+    }
+}
