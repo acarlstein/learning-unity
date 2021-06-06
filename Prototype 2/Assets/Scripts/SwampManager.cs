06a22f13d599942db23ba22b19017b7e46c43410
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-05 19:00:39 -0600

    Added Box Collision to animals and bullet, triggered. Made the bullet rigit without gravity

--- Prototype 2/Assets/Scripts/SwampManager.cs
+++ Prototype 2/Assets/Scripts/SwampManager.cs
@@ -9,19 +9,32 @@ public class SwampManager : MonoBehaviour
     public float rangeX = 20;
     private readonly float positionZ = 20;
 
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAninmal", startDelay, spawnInterval);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            float positionX = Random.Range(-rangeX, rangeX);
-            Vector3 spawnPosition = new Vector3(positionX, 0, positionZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(
-                animalPrefabs[animalIndex],
-                spawnPosition, 
-                animalPrefabs[animalIndex].transform.rotation
-            );
+            SpawnRandomAninmal();
         }        
+    }
+
+    void SpawnRandomAninmal()
+    {
+        float positionX = Random.Range(-rangeX, rangeX);
+        Vector3 spawnPosition = new Vector3(positionX, 0, positionZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(
+            animalPrefabs[animalIndex],
+            spawnPosition,
+            animalPrefabs[animalIndex].transform.rotation
+        );
     }
 }
