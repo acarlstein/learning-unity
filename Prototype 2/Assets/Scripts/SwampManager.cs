d0a8534b7e8a40c22af09906c8d5e1fbe294e3ff
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-05 18:37:01 -0600

    Randomize start position from the X axis of the swamp of animals

--- Prototype 2/Assets/Scripts/SwampManager.cs
+++ Prototype 2/Assets/Scripts/SwampManager.cs
@@ -6,16 +6,20 @@ public class SwampManager : MonoBehaviour
 {
 
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    public float rangeX = 20;
+    private readonly float positionZ = 20;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            float positionX = Random.Range(-rangeX, rangeX);
+            Vector3 spawnPosition = new Vector3(positionX, 0, positionZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(
-                animalPrefabs[animalIndex], 
-                new Vector3(0, 0, 20), 
+                animalPrefabs[animalIndex],
+                spawnPosition, 
                 animalPrefabs[animalIndex].transform.rotation
             );
         }        
