5d20bac424541c2a53cf6446e788ec0c30318162
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-05 18:23:54 -0600

    Swamp animals with keyboard for testing

---
+++ Prototype 2/Assets/Scripts/SwampManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(
+                animalPrefabs[animalIndex], 
+                new Vector3(0, 0, 20), 
+                animalPrefabs[animalIndex].transform.rotation
+            );
+        }        
+    }
+}
