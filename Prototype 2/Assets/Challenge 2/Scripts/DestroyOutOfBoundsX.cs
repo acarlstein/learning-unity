0631bff4a18919fff8022bd4f695f8abcb00537c
Alejandro Carlstein Ramos Mejia <alejandro@carlsteinrm.com>
2021-06-08 17:18:52 -0600

    Creating Second Lab Exercise

--- Prototype 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -12,15 +12,9 @@ public class DestroyOutOfBoundsX : MonoBehaviour
     {
         // Destroy dogs if x position less than left limit
         // Destroy balls if y position less than bottom limit
-        if (transform.name.StartsWith("Dog"))
-        {
-            Debug.Log(transform.name + ", X:" + transform.position.x);
-        }
-
         if (transform.position.x < leftLimit
             || transform.position.y < bottomLimit)
         {
-            Debug.Log(gameObject.name + " Destroyed at X:" + transform.position.x + ",Y: "+transform.position.y);
             Destroy(gameObject);
         }
 
